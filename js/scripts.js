6ffdbbb4334f9576976cd380e7c531bf1b7bd829
Isaac Shoman <isaac.shoman@gmail.com>
2018-03-23 10:41:36 -0700

    Add first draft of front-end js logic

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,16 @@
 
 // Front-end Logic
 $(document).ready(function() {
+  $("#suggestion").submit(function(event) {
+    event.preventDefault();
+    var q1 = parseInt($("#q1").val());
+    var q2 = parseInt($("#q1").val());
+    var q3 = parseInt($("#q1").val());
+    var q4 = parseInt($("#q1").val());
+    var q5 = parseInt($("#q1").val());
+    var result = suggestion(q1, q2, q3, q4, q5);
+    console.log (result)
+  });
 
 
 
